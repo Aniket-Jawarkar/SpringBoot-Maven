a95c480c639603a7ca6f5c89fcabd8ae42309c8a
aniketjawarkar <aniketnot2026@gmail.com>
2025-06-27 21:48:23 +0530

    initial commit

---
+++ backend/src/test/java/com/aniket/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniket.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
